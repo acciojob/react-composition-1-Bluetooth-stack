0403aea92001fc58d59b35730ec549b0e91fd711
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-17 19:43:45 +0530

    initial

---
+++ src/components/Tabs.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const arrOfTab = [
+    {
+        title: "Tab 1",
+        content: "This is the content of Tab 1"
+    },
+    {
+        title: "Tab 2",
+        content: "This is the content of Tab 2"
+    },
+    {
+        title: "Tab 3",
+        content: "This is the content of Tab 3"
+    }
+]
+
+const Tabs = () => {
+    // let [tab, setTab] = useState(arrOfTab);
+    let [content, setContent] = useState("");
+    function showContent(obj) {
+        setContent(obj.content);
+    }
+
+
+    return (
+        <div>
+            {
+                arrOfTab.map((obj) => (
+                    <li onClick={()=>{showContent(obj)}}>
+                        {obj.title}
+                    </li>
+                ))
+            }
+
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Tabs;
